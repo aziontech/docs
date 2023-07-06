f821abf62d7eb386d9a2ebaf634b0bb0714f713e
gabriel-azion <gabriel.franca@azion.com>
2023-07-06 12:26:16 -0300

    menu lateral esquerdo

--- src/i18n/en/nav.ts
+++ src/i18n/en/nav.ts
@@ -17,43 +17,48 @@ export default [
 
 
 	{ text: 'Accounts and Profile', header: true, type: 'learn', key: 'accountsProfile' },
-	{ text: 'Create account', slug: 'documentation/products/accounts/creating-account', key: 'accountsProfile/createAccount' },
-	{ text: 'Acccount Settings', slug: 'documentation/products/accounts/account-settings', key: 'accountsProfile/settings' },
+	{ text: 'Create Account', slug: 'documentation/products/accounts/creating-account', key: 'accountsProfile/createAccount' },
+	{ text: 'Account Settings', slug: 'documentation/products/accounts/account-settings', key: 'accountsProfile/settings' },
 	{ text: 'Users Managements', slug: 'documentation/products/accounts/users-and-teams', key: 'accountsProfile/usersManagement' },
 	{ text: 'Billing', slug: 'documentation/products/accounts/billing', key: 'accountsProfile/billing' },
 	{ text: 'Credentials', slug: 'documentation/products/credentials', key: 'accountsProfile/credentials' },
 	{ text: 'Activity History', slug: 'documentation/products/accounts/activity-history', key: 'accountsProfile/activityHistory' },
 	{ text: 'Teams Permissions', slug: 'documentation/products/accounts/teams-permissions', key: 'accountsProfile/teamsPermissions' },
 	{ text: 'Single Sign-On (SSO)', slug: 'documentation/products/accounts/single-sign-on', key: 'accountsProfile/singleSignOn' },
-	{ text: 'Your settings', slug: 'documentation/products/accounts/your-settings', key: 'accountsProfile/yourSettings' },
+	{ text: 'Your Settings', slug: 'documentation/products/accounts/your-settings', key: 'accountsProfile/yourSettings' },
 	{ text: 'Personal Tokens', slug: 'documentation/products/accounts/personal-tokens', key: 'accountsProfile/personalTokens' },
 	{ text: 'Social Login', slug: 'documentation/products/accounts/social-login', key: 'accountsProfile/socialLogin' },
 	{ text: 'Groups and Clients', slug: 'documentation/products/accounts/clients', key: 'accountsProfile/GroupsAndClients' },
 	{ text: 'Multi-Factor Authentication', slug: 'documentation/products/accounts/multi-factor-authentication', key: 'accountsProfile/multiFactorAuth' },
 
 	{ text: 'Build', header: true, type: 'learn', key: 'Build' },
-	{ text: 'Edge Application', slug: 'documentation/products/edge-application/application-acceleration', key: 'edgeApplication' },
+	{ text: 'Edge Application', slug: 'documentation/products/edge-application', key: 'edgeApplication' },
+	{ text: 'Application Acceleration', slug: 'documentation/products/edge-application/application-acceleration', key: 'applicationAcceleration' },
 	{ text: 'Edge Caching', slug: 'documentation/products/edge-application/edge-caching', key: 'edgeCaching' },
-	{ text: 'Edge Function', slug: 'documentation/products/edge-application/edge-functions', key: 'edgeFunctions' },
+	{ text: 'Edge Functions', slug: 'documentation/products/edge-application/edge-functions', key: 'edgeFunctions' },
 	{ text: 'Image Processor', slug: 'documentation/products/edge-application/image-processor', key: 'imageProcessor' },
 	{ text: 'Load Balancer', slug: 'documentation/products/edge-application/load-balancer', key: 'loadBalancer' },
 
 	{ text: 'Secure', header: true, type: 'learn', key: 'Secure' },
+	{ text: 'Edge Firewall', slug: 'documentation/products/edge-firewall', key: 'edgefirewall' },
 	{ text: 'DDoS Protection', slug: 'documentation/products/edge-firewall/ddos-protection', key: 'ddosProtection' },
 	{ text: 'Network Layer Protection', slug: 'documentation/products/edge-firewall/network-layer-protection', key: 'networkLayerProtection' },
 	{ text: 'Web Application Firewall', slug: 'documentation/products/edge-firewall/web-application-firewall', key: 'webApplicationFirewall' },
+	{ text: 'Intelligent DNS', slug: 'documentation/products/intelligent-dns', key: 'intelligentDNS' },
+
 
 	{ text: 'Deliver', header: true, type: 'learn', key: 'Deliver' },
+	{ text: 'Edge Orchestrator', slug: 'documentation/products/edge-orchestrator', key: 'edgeOrchestrator' },
 	{ text: 'Edge Node', slug: 'documentation/products/edge-orchestrator/edge-node', key: 'edgeNode' },
 	{ text: 'Edge Services', slug: 'documentation/products/edge-orchestrator/edge-services', key: 'edgeServices' },
 
 	{ text: 'Observe', header: true, type: 'learn', key: 'Observe' },
 	{ text: 'Data Streaming', slug: 'documentation/products/data-streaming', key: 'dataStreaming' },
 	{ text: 'Edge Pulse', slug: 'documentation/products/edge-pulse', key: 'edgePulse' },
-	{ text: 'Real-Time Metrics', slug: 'documentation/products/real-time-metrics', key: 'realTimeMetrics' },
 	{ text: 'Real-Time Events', slug: 'documentation/products/real-time-events', key: 'realTimeEvents' },
+	{ text: 'Real-Time Metrics', slug: 'documentation/products/real-time-metrics', key: 'realTimeMetrics' },
 
-	{ text: 'Marketplace', header: true, anchor: true, type: 'learn', slug: 'documentation/products/marketplace', key: 'Marketplace' },
+	{ text: 'Marketplace', header: true, anchor: true, type: 'learn', slug: 'documentation/products/marketplace', key: 'marketplace' },
 
 	{ text: 'Services', header: true, type: 'learn', key: 'Services' },
 	{ text: 'Integration Services', slug: 'documentation/services/solutions-lab/integration-services', key: 'integrationServices' },
@@ -63,7 +68,9 @@ export default [
 	{ text: 'API', header: true, anchor: true, type: 'learn', slug: 'https://api.azion.com', key: 'api' },
 	{ text: 'GraphQL API', header: true, anchor: true, type: 'learn', slug: 'documentation/products/graphql-api', key: 'graphQLAPI' },
 	{ text: 'Azion CLI', header: true, anchor: true, type: 'learn', slug: 'documentation/products/cli', key: 'azionCLI' },
-	{ text: 'Edge Runtime', header: true, anchor: true, type: 'learn', slug: 'documentation/products/cli', key: 'edgeRuntime' },
+	{ text: 'Edge Runtime', header: true, anchor: true, type: 'learn', slug: 'documentation/products/edge-application/edge-functions/runtime/overview', key: 'edgeRuntime' },
+	{ text: 'SDK', header: true, anchor: true, type: 'learn', slug: 'documentation/documentation/devtools/sdk/go/', key: 'sdkGO' },
+	{ text: 'Terraform', header: true, anchor: true, type: 'learn', slug: 'documentation/products/terraform-provider/', key: 'terraform' },
 
 
 	{ text: 'Changelog', header: true, type: 'learn', anchor: true, slug: 'documentation/products/changelog', key: 'changelog' },
--- src/i18n/pt-br/nav.ts
+++ src/i18n/pt-br/nav.ts
@@ -1,9 +1,70 @@
 import { NavDictionary } from '../translation-checkers';
 
 export default NavDictionary([
-	{ key: 'accountsProfile', text: 'Comece aqui' },
-	{ key: 'accountsProfile/createAccount', text: 'Introdução', slug: 'documentacao' },
-	{ key: 'accountsProfile/settings', text: 'Configurações de Conta' },
+
+	{ key: 'accountsProfile/settings', text: 'Configurações de Conta', slug: 'documentacao/produtos/gestao-de-contas'},
+	
+
+	{ text: 'Contas e Perfil', key: 'accountsProfile' },
+	{ text: 'Criar uma Conta', slug: 'documentacao/produtos/gestao-de-contas/criar-conta', key: 'accountsProfile/createAccount' },
+	{ text: 'Account Settings', slug: 'documentacao/produtos/gestao-de-contas/account-settings', key: 'accountsProfile/settings' },
+	{ text: 'Users Managements', slug: 'documentacao/produtos/gestao-de-contas/usuarios-e-times', key: 'accountsProfile/usersManagement' },
+	{ text: 'Faturamento', slug: 'documentacao/produtos/gestao-de-contas/faturamento', key: 'accountsProfile/billing' },
+	{ text: 'Credenciais', slug: 'documentacao/produtos/credenciais/', key: 'accountsProfile/credentials' },
+	{ text: 'Activity History', slug: 'documentacao/produtos/gestao-de-contas/activity-history', key: 'accountsProfile/activityHistory' },
+	{ text: 'Teams Permissions', slug: 'documentacao/produtos/gestao-de-contas/teams-permissions', key: 'accountsProfile/teamsPermissions' },
+	{ text: 'Single Sign-On (SSO)', slug: 'documentacao/produtos/gestao-de-contas/single-sign-on', key: 'accountsProfile/singleSignOn' },
+	{ text: 'Your Settings', slug: 'documentacao/produtos/gestao-de-contas/your-settings', key: 'accountsProfile/yourSettings' },
+	{ text: 'Personal Tokens', slug: 'documentacao/produtos/gestao-de-contas/personal-tokens', key: 'accountsProfile/personalTokens' },
+	{ text: 'Login Social', slug: 'documentacao/produtos/gestao-de-contas/social-login', key: 'accountsProfile/socialLogin' },
+	{ text: 'Grupos e Clientes', slug: 'documentacao/produtos/gestao-de-contas/clients', key: 'accountsProfile/GroupsAndClients' },
+	{ text: 'Multi-Factor Authentication', slug: 'documentacao/produtos/gestao-de-contas/multi-factor-authentication', key: 'accountsProfile/multiFactorAuth' },
+
+	//{ text: 'Build', header: true, type: 'learn', key: 'Build' },
+	{ text: 'Edge Application', slug: 'documentacao/produtos/edge-application', key: 'edgeApplication' },
+	{ text: 'Application Acceleration', slug: 'documentacao/produtos/edge-application/application-acceleration', key: 'applicationAcceleration' },
+	{ text: 'Edge Caching', slug: 'documentacao/produtos/edge-application/edge-caching', key: 'edgeCaching' },
+	{ text: 'Edge Functions', slug: 'documentacao/produtos/edge-application/edge-functions', key: 'edgeFunctions' },
+	{ text: 'Image Processor', slug: 'documentacao/produtos/edge-application/image-processor', key: 'imageProcessor' },
+	{ text: 'Load Balancer', slug: 'documentacao/produtos/edge-application/load-balancer', key: 'loadBalancer' },
+
+	//{ text: 'Secure', header: true, type: 'learn', key: 'Secure' },
+	{ text: 'Edge Firewall', slug: 'documentacao/produtos/edge-firewall', key: 'edgefirewall' },
+	{ text: 'DDoS Protection', slug: 'documentacao/produtos/edge-firewall/ddos-protection', key: 'ddosProtection' },
+	{ text: 'Network Layer Protection', slug: 'documentacao/produtos/edge-firewall/network-layer-protection', key: 'networkLayerProtection' },
+	{ text: 'Web Application Firewall', slug: 'documentacao/produtos/edge-firewall/web-application-firewall', key: 'webApplicationFirewall' },
+	{ text: 'Intelligent DNS', slug: 'documentacao/produtos/intelligent-dns', key: 'intelligentDNS' },
+
+
+	//{ text: 'Deliver', header: true, type: 'learn', key: 'Deliver' },
+	{ text: 'Edge Orchestrator', slug: 'documentacao/produtos/edge-orchestrator', key: 'edgeOrchestrator' },
+	{ text: 'Edge Node', slug: 'documentacao/produtos/edge-orchestrator/edge-node', key: 'edgeNode' },
+	{ text: 'Edge Services', slug: 'documentacao/produtos/edge-orchestrator/edge-services', key: 'edgeServices' },
+
+	//{ text: 'Observe', header: true, type: 'learn', key: 'Observe' },
+	{ text: 'Data Streaming', slug: 'documentacao/produtos/data-streaming', key: 'dataStreaming' },
+	{ text: 'Edge Pulse', slug: 'documentacao/produtos/edge-pulse', key: 'edgePulse' },
+	{ text: 'Real-Time Events', slug: 'documentacao/produtos/real-time-events', key: 'realTimeEvents' },
+	{ text: 'Real-Time Metrics', slug: 'documentacao/produtos/real-time-metrics', key: 'realTimeMetrics' },
+
+	//{ text: 'Marketplace', header: true, anchor: true, type: 'learn', slug: 'documentation/products/marketplace', key: 'marketplace' },
+
+	{ text: 'Serviços', key: 'Services' },
+	{ text: 'Integration Services', slug: 'documentacao/servicos/solutions-lab/integration-services', key: 'integrationServices' },
+	{ text: 'Suporte', slug: 'documentacao/servicos/suporte', key: 'support' },
+
+
+	{ text: 'API',slug: 'https://api.azion.com', key: 'api' },
+	{ text: 'GraphQL API', slug: 'documentacao/produtos/graphql-api/', key: 'graphQLAPI' },
+	{ text: 'Azion CLI', slug: 'documentacao/produtos/cli/', key: 'azionCLI' },
+	{ text: 'Edge Runtime', slug: 'documentacao/produtos/edge-application/edge-functions/runtime/visao-geral/', key: 'edgeRuntime' },
+	{ text: 'SDK', slug: 'documentacao/devtools/sdk/go/', key: 'sdkGO' },
+	{ text: 'Terraform', slug: 'documentacao/produtos/terraform-provider/', key: 'terraform' },
+
+	{ text: 'Changelog', slug: 'documentacao/produtos//changelog', key: 'changelog' },
+	{ text: 'System Status', slug: 'https://status.azion.com', key: 'systemStatus' },
+	{ text: 'Azion Network Program', slug: 'documentacao/produtos/azion-network-program', key: 'azionNetworkProgram' },
+
 	// install: 'Instalação',
 	// 'editor-setup': 'Configuração do Editor',
 	// 'guides/upgrade-to/v2': 'Atualize para a v2',
