4e1a0e9112ff4c3e6b2eacc794d6d7766fa95903
gabriel-azion <gabriel.franca@azion.com>
2023-07-06 17:14:18 -0300

    graphQL devtools menu

--- src/i18n/pt-br/graphqlMenu.ts
+++ src/i18n/pt-br/graphqlMenu.ts
@@ -1,32 +1,25 @@
-/**
- * This configures the navigation sidebar.
- * All other languages follow this ordering/structure and will fall back to
- * English for any entries they haven’t translated.
- *
- * - All entries MUST include `text` and `key`
- * - Heading entries MUST include `header: true` and `type`
- * - Link entries MUST include `slug` (which excludes the language code)
- */
- export default [
-	{ text: 'Documentation', header: true, onlyMobile: true, anchor: true, type: 'learn', slug: 'documentation', key: 'documentation' },
-	{ text: 'Guides', header: true, onlyMobile: true, anchor: true, type: 'learn', slug: 'guides', key: 'guides' },
-	{ text: 'Dev Tools', header: true, onlyMobile: true, anchor: true, type: 'learn', slug: 'devTools', key: 'devTools' },
-	{ text: 'Learn', header: true, onlyMobile: true, anchor: true, type: 'learn', slug: 'Learn', key: 'Learn' },
+import { NavDictionary } from '../translation-checkers';
+
+ export default NavDictionary([
+	{ text: 'Documentação',  slug: 'documentation', key: 'documentation' },
+	{ text: 'Guias',  slug: 'guides', key: 'guides' },
+	{ text: 'Dev Tools',  slug: 'devTools', key: 'devTools' },
+	{ text: 'Learn',  slug: 'Learn', key: 'Learn' },
 
 	/// START HERE :::: DO NOT REMOVE the strings above, it's a work around for header on mobile /// 
 
-	{ text: 'Overview', header: true, anchor: true, type: 'learn', key: 'overview', slug: '/documentation/products/graphql-api-overview' },
-	{ text: 'First Steps', header: true, anchor: true, type: 'learn', key: 'firstSteps', slug: '/documentation/products/graphql-api-first-steps' },
-	{ text: 'Queries', header: true, anchor: true, type: 'learn', slug: '/documentation/products/graphql-api-queries', key: 'queries' },
-	{ text: 'Queries Examples',header: true, anchor: true, type: 'learn', slug: '/documentation/products/graphql-api-queries-examples', key: 'queries/examples' },
-	{ text: 'Features',header: true, anchor: true, type: 'learn', slug: '/documentation/products/graphql-api-features', key: 'features' },
-	{ text: 'Limits', header: true, anchor: true, type: 'learn', slug: '/documentation/products/graphql-api-limits', key: 'limits' },
-	{ text: 'Error Responses', header: true, anchor: true, type: 'learn', slug: '/documentation/products/graphql-api-error-responses', key: 'queries/error-responses' },
+	{ text: 'Visão geral', key: 'overview', slug: '/documentacao/produtos/graphql-api/visao-geral' },
+	{ text: 'Primeiros Passos', key: 'firstSteps', slug: '/documentacao/produtos/graphql-api-primeiros-passos' },
+	{ text: 'Queries', slug: '/documentacao/produtos/graphql-api-queries', key: 'queries' },
+	{ text: 'Exemplos de queries', slug: '/documentacao/produtos/graphql-api-exemplos-queries', key: 'queries/examples' },
+	{ text: 'Recursos', slug: '/documentacao/produtos/graphql-api-recursos', key: 'features' },
+	{ text: 'Limites', slug: '/documentacao/produtos/graphql-api-limites', key: 'limits' },
+	{ text: 'Mensagens de erro', slug: '/documentacao/produtos/graphql-api-mensagens-erro', key: 'queries/error-responses' },
 
-	{ text: 'Guides', header: true, type: 'learn', key: 'guides' },
-	{ text: 'How to query GraphQL requests on Postman', slug: '/documentation/products/guides/query-graphql-postman', key: 'guides/graphql-postman' },
-	{ text: 'How to query metadata with GraphQL API', slug: '/documentation/products/guides/graphql-metadata', key: 'guides/graphql-metadata' },
-	{ text: 'How to query aggregated data with GraphQL API', slug: '/documentation/products/guides/graphql-aggregated-data', key: 'guides/graphql-aggregated-data' },
-	{ text: 'How to select Top X queries with GraphQL API', slug: '/documentation/products/guides/graphql-top-x-query', key: 'guides/graphql-top-x' },
+	{ text: 'Guias', key: 'guides' },
+	{ text: 'Como rodar requisições da GraphQL no Postman', slug: '/documentacao/produtos/guias/consultar-graphql-postman', key: 'guides/graphql-postman' },
+	{ text: 'Como consultar metadados com a GraphQL API', slug: '/documentacao/produtos/guias/graphql-metadados', key: 'guides/graphql-metadata' },
+	{ text: 'Como realizar consultas agregando dados com a GraphQL API', slug: '/documentacao/produtos/guias/graphql-dados-agregados', key: 'guides/graphql-aggregated-data' },
+	{ text: 'Como selecionar Top X queries com a GraphQL API', slug: '/documentacao/produtos/guias/graphql-query-top-x', key: 'guides/graphql-top-x' },
 
-] as const;
+]);
