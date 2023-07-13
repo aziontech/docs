95e876593fb6a45568112d2eceda0d9cb47c8be9
Luis Igreja <lfsigreja@gmail.com>
2023-07-13 11:08:10 -0300

    chore: link external for header links + style

--- src/components/Header/Menus.tsx
+++ src/components/Header/Menus.tsx
@@ -18,21 +18,37 @@ interface Props {
 const Navigation = ({ data }: Props) => {
 	return (
 		<nav className={"menu-navigation"}>
-			{data.map((i) => {
+			{data.map(({ title, href, target, dropdown }) => {
 				return (
-					<ul key={i.title} className="menu-list">
+					<ul key={title} className="menu-list">
 						<li className={'menu-list-item'}>
-							<a href={i.href} className={'menu-link'} target={i.target ? i.target : "_self"}>
-								{i.title}
+							<a href={href} className={'menu-link'} target={target ? target : "_self"}>
+								{title}
+								{href?.includes('https') &&
+									<svg width="12" height="12" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+										<path
+											fill="white"
+											d="M5.50098 5H17.9552V3H5.00098C4.17255 3 3.50098 3.67157 3.50098 4.5V35.5C3.50098 36.3284 4.17255 37 5.00098 37H36.001C36.8294 37 37.501 36.3284 37.501 35.5V24.0096H35.501V35H5.50098V5ZM34.0868 5H25.9818V3H36.501C37.0533 3 37.501 3.44772 37.501 4V16.0248H35.501V6.41421L21.2539 20.6613L19.8397 19.2471L34.0868 5Z"
+										/>
+									</svg>
+								}
 							</a>
-							{i.dropdown ? (
+							{dropdown ? (
 								<div className={"dropdown-area dev-tools"}>
 									<ul className={"dropdown-list"}>
-										{i.dropdown.map((y) => {
+										{dropdown.map(({ title, href, target }) => {
 											return (
-												<li key={y.title} className={"dropdown-list-item"}>
-													<a href={y.href} target={y.target ? y.target : "_self"}>
-														{y.title}
+												<li key={title} className={"dropdown-list-item"}>
+													<a href={href} target={target ? target : "_self"}>
+														{title}
+														{href?.includes('https') &&
+															<svg width="12" height="12" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+																<path
+																	fill="white"
+																	d="M5.50098 5H17.9552V3H5.00098C4.17255 3 3.50098 3.67157 3.50098 4.5V35.5C3.50098 36.3284 4.17255 37 5.00098 37H36.001C36.8294 37 37.501 36.3284 37.501 35.5V24.0096H35.501V35H5.50098V5ZM34.0868 5H25.9818V3H36.501C37.0533 3 37.501 3.44772 37.501 4V16.0248H35.501V6.41421L21.2539 20.6613L19.8397 19.2471L34.0868 5Z"
+																/>
+															</svg>
+														}
 													</a>
 												</li>
 											)
