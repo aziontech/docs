8987f16df28fe6872c3c80c76d89b7934fe448a4
gabriel-azion <gabriel.franca@azion.com>
2023-07-13 15:21:07 -0300

    add menu devtools

---
+++ src/i18n/pt-br/devtoolsMenu.ts
@@ -0,0 +1,25 @@
+/**
+ * This configures the navigation sidebar.
+ * All other languages follow this ordering/structure and will fall back to
+ * English for any entries they haven't translated.
+ *
+ * - All entries MUST include `text` and `key`
+ * - Heading entries MUST include `header: true` and `type`
+ * - Link entries MUST include `slug` (which excludes the language code)
+ */
+ export default [
+	{ text: 'Documentação', header: true, onlyMobile: true, anchor: true, slug: 'documentation', key: 'documentation' },
+	{ text: 'Guias',header: true, onlyMobile: true, anchor: true, slug: 'guides', key: 'guides' },
+	{ text: 'Dev Tools',header: true, onlyMobile: true, anchor: true, slug: 'devTools', key: 'devTools' },
+	{ text: 'Learn',header: true, onlyMobile: true, anchor: true, slug: 'Learn', key: 'Learn' },
+
+	/// START HERE :::: DO NOT REMOVE the strings above, it's a work around for header on mobile /// 
+
+	{ text: 'CLI', header: true, anchor: true, type: 'learn', key: 'devtools/cli', slug: '/documentacao/produtos/cli/' },
+	{ text: 'API', header: true, anchor: true, type: 'learn', key: 'devtools/api', slug: 'https://api.azion.com/' },
+	{ text: 'API GraphQL', header: true, anchor: true, type: 'learn', slug: '/documentacao/produtos/graphql-api/', key: 'devtools/graphQL' },
+	{ text: 'Edge Runtime',header: true, anchor: true, type: 'learn', slug: '/documentacao/produtos/edge-application/edge-functions/runtime/visao-geral/', key: 'devtools/runtime' },
+	{ text: 'SDK',header: true, anchor: true, type: 'learn', slug: '/documentacao/devtools/sdk/go/', key: 'devtools/sdk' },
+	{ text: 'Terraform', header: true, anchor: true, type: 'learn', slug: '/documentacao/produtos/terraform-provider/', key: 'devtools/terraform' },
+
+] as const;
