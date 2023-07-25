81a95b2d54192861dccc19cc56bba9fdef90fde6
Robson Junior <robson.ga.junior@gmail.com>
2023-07-25 11:13:20 -0300

    WIP: update theme color

--- integrations/expressive-code.ts
+++ integrations/expressive-code.ts
@@ -1,4 +1,4 @@
-import { pluginFramesTexts, } from 'astro-expressive-code';
+import { pluginFramesTexts } from 'astro-expressive-code';
 import { translations } from '../src/i18n/util';
 
 Object.entries(translations).forEach(([locale, texts]) => {
@@ -9,14 +9,23 @@ Object.entries(translations).forEach(([locale, texts]) => {
 	});
 });
 
+
+/**
+ * dark-plus, dracula-soft, dracula, github-dark-dimmed, github-dark,
+ * github-light, hc_light, light-plus, material-theme-darker,
+ * material-theme-lighter, material-theme-ocean, material-theme-palenight,
+ * material-theme, min-dark, min-light, monokai, nord, one-dark-pro,
+ * poimandres, rose-pine-dawn, rose-pine-moon, rose-pine, slack-dark,
+ * slack-ochin, solarized-dark, solarized-light, vitesse-dark, vitesse-light
+ */
 export const theme = {
-	theme: 'slack-dark',
+	theme: 'vitesse-dark',
 	styleOverrides: {
 		codeFontFamily: 'var(--font-mono)'
 	},
-	frames: {
-		styleOverrides: {
-			editorTabBarBackground: 'var(--color-gray-80)',
-		}
-	}
+	// frames: {
+	// 	styleOverrides: {
+	// 		editorTabBarBackground: 'var(--color-gray-80)',
+	// 	}
+	// }
 };
