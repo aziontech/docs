06326ff69c6234439a6ee5700642ff0ea4d0dfed
Luis Igreja <lfsigreja@gmail.com>
2023-08-23 15:42:21 -0300

    chore: new menu layout

--- src/components/LeftSidebar/SidebarContent.tsx
+++ src/components/LeftSidebar/SidebarContent.tsx
@@ -1,4 +1,4 @@
-import { isSubPage, isSubMenu } from '~/util/isSubPage';
+import { isSubPage } from '~/util/isSubPage';
 import { modelSlug } from '~/util';
 import ExternalLinkIcon from '~/components/icons/ExternalLinkIcon';
 import './SidebarContent.scss'
@@ -10,50 +10,72 @@ interface Props {
 	currentPageMatch: string,
 }
 
-const MenuItems = ({data, lang, currentPageMatch}: Props ) => {
-	const renderMenuItems = (data: NavDict | undefined) => {
+const MenuItems = ({ data, lang, currentPageMatch }: Props) => {
+	const createListItems = (data: NavDict) => {
 		return (
-				<ul>
-					{data && data.map(({ slug, text, isFallback, children }) => (
-						<li key={text} className="nav-link">
-						{children ? (
-							<details open={isSubMenu(currentPageMatch, lang, children) ? true : false}>
-								<summary>
-									<h2 className={isSubMenu(currentPageMatch, lang, children) ? 'focus-item' : ''}>
-										{text}
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 1 16 16"
-											width="16"
-											height="16"
-											aria-hidden="true"
-										>
-											<path d="M6.22 3.22a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06L9.94 8 6.22 4.28a.75.75 0 010-1.06z" />
-										</svg>
-									</h2>
-								</summary>
-								<ul>{renderMenuItems(children)}</ul>
-							</details>
-						) : (
-							<a
-								className="nav-item-children"
-								rel="noreferrer"
-								href={modelSlug(slug, isFallback, lang)} target={slug?.includes('https') ? "_blank" : "_self"}
-								aria-current={isSubPage(currentPageMatch, slug, lang) ? "page" : "false"}
-								data-current-parent={isSubPage(currentPageMatch, slug, lang) ? "true" : "false"}
-							>
-								{text}
-								{isFallback && <sup className="fallback">EN</sup>}
-								{slug?.includes('https') && <ExternalLinkIcon color="var(--theme-text)" />}
-							</a>
-							)}
-						</li>
-					))}
+			<ul className="list-border-left">
+				{data.map(({ slug, text, isFallback }) => (
+					<li data-current-page={isSubPage(currentPageMatch, slug, lang) ? "true" : "false"} key={text} className="nav-link">
+						<a
+							rel="noreferrer"
+							href={modelSlug(slug, isFallback, lang)} target={slug?.includes('https') ? "_blank" : "_self"}
+							aria-current={isSubPage(currentPageMatch, slug, lang) ? "page" : "false"}
+							data-current-parent={isSubPage(currentPageMatch, slug, lang) ? "true" : "false"}
+						>
+							{text}
+							{isFallback && <sup className="fallback">EN</sup>}
+							{slug?.includes('https') && <ExternalLinkIcon color="var(--theme-text)" />}
+						</a>
+					</li>
+				))}
 			</ul>
 		);
+	}
+	const renderMenuItems = (data: NavDict) => {
+		let hasHighlight = false
+		const mappedData = data.reduce((array, item) => {
+			if ('isProduct' in item) {
+				hasHighlight = true
+				array.push({ ...item, children: [] })
+			} else if (array.length > 0 && 'children' in array[array.length - 1]) {
+				array[array.length - 1].children?.push(item)
+			} else {
+				array.push(item)
+			}
+
+			return array
+		}, [] as NavDict)
+
+		if (hasHighlight) {
+			return (
+				<ul>
+					{mappedData.map(item => (
+						<li key={item.text}>
+							<a 
+								className="nav-link-highlight"
+								rel="noreferrer"
+								href={modelSlug(item.slug, item.isFallback, lang)} 
+								target={item.slug?.includes('https') ? "_blank" : "_self"}
+								aria-current={isSubPage(currentPageMatch, item.slug, lang) ? "page" : "false"}
+								data-current-parent={isSubPage(currentPageMatch, item.slug, lang) ? "true" : "false"}
+							>
+								{item.text}
+								{item.isFallback && <sup className="fallback">EN</sup>}
+								{item.slug?.includes('https') && <ExternalLinkIcon color="var(--theme-text)" />}
+							</a>
+							{item.children && createListItems(item.children)}
+						</li>
+					))}
+				</ul>
+			)
+		}
+
+		return <>{createListItems(mappedData)}</>
 	};
 
-return <>{renderMenuItems(data)}</>;
+	if (!data) return null
+
+	return <>{renderMenuItems(data)}</>;
 };
 
 export default MenuItems;
