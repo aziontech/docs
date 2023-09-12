95854ece64be2ed4a5bd3d1af05d23a581a0c9d1
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2023-09-12 15:01:45 -0300

    feat: edit on github

--- src/util/getGithubEditUrl.ts
+++ src/util/getGithubEditUrl.ts
@@ -7,13 +7,14 @@ export function getGithubEditUrl(Astro: Readonly<AstroGlobal>) {
 	const { content = {} } = Astro.props;
 	const isFallback = !!Astro.params.fallback;
 	const currentPage = Astro.url.pathname;
+	const currentFileId = Astro.props.id;
 	const lang = getLanguageFromURL(currentPage);
-	const filePath = `src/content/docs${currentPage.replace(/\/$/, '')}.mdx`;
+	const filePath = `src/content/docs/${currentFileId.replace(/\/$/, '')}`;
 	const currentFile = isFallback ? filePath.replace(`/${lang}/`, '/en/') : filePath;
 	const githubEditUrl =
 		content.githubURL && (lang === 'en' || isFallback)
 			? `${content.githubURL}${content.hasREADME ? 'README.md' : ''}`
-			: `https://github.com/withastro/docs/blob/main/${currentFile}`;
+			: `https://github.com/aziontech/docs/blob/main/${currentFile}`;
 
 	return githubEditUrl;
 }
