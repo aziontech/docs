024dbad8f6432f9102d1e7183f22d3ab5cced00d
Luis Igreja <lfsigreja@gmail.com>
2023-09-13 16:22:04 -0300

    chore: new xml doc file + better data mapper

--- src/pages/[lang]/doc-all-data.json.js
+++ src/pages/[lang]/doc-all-data.json.js
@@ -2,10 +2,14 @@ import { allPages } from "~/content";
 import { removeTrailingSlash, removeLeadingSlash } from "~/util";
 import { groupPagesByLang } from "~/util/groupPagesByLang";
 
+const langs = ['en', 'pt-br']
+const data = {}
 const pagesData = groupPagesByLang(allPages)
 
-const data = {
-	en: pagesData['en'].map(page => {
+langs.forEach((lang) => {
+	if (!pagesData[lang]) return
+	
+	data[lang] = pagesData[lang].map(page => {
 		return {
 			repository: 'docs',
 			title: page.data.title,
@@ -15,19 +19,8 @@ const data = {
 			noindex: false,
 			url: `https://www.azion.com/en/${removeTrailingSlash(removeLeadingSlash(page.data.permalink))}/`
 		}
-	}),
-	"pt-br": pagesData["pt-br"].map(page => {
-		return {
-			repository: 'docs',
-			title: page.data.title,
-			namespace: page.data.namespace,
-			description: page.data.description,
-			og_image: page.data.og_image,
-			noindex: false,
-			url: `https://www.azion.com/pt-br/${removeTrailingSlash(removeLeadingSlash(page.data.permalink))}/`
-		}
 	})
-}
+})
 
 export async function get({ params }) {
 	const lang = params.lang;
