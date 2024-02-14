f37cfb117796bca8f535ab34c990258004037450
gabriel.azion <gabriel.franca@azion.com>
2024-02-14 09:50:00 -0300

    separate init build and deploy

--- src/i18n/pt-br/cliMenuAlpha.ts
+++ src/i18n/pt-br/cliMenuAlpha.ts
@@ -15,8 +15,9 @@ export default [
 
 	{ text: 'Visão geral', header: true, anchor: true, type: 'learn', key: 'overview', slug: '/documentacao/produtos/azion-cli/visao-geral/', hasLabel:'menu.cli' },
 
-	{ text: 'Init, build e deploy', header: true, anchor: true, slug: '/documentacao/produtos/azion-cli/criar-application/', key: 'cli/createJourney' },
-	{ text: 'Dev', header: true, anchor: true, slug: '/documentacao/produtos/azion-cli/dev-comando/', key: 'cli/localDev' },
+	{ text: 'Init', header: true, anchor: true, slug: '/documentacao/devtools/cli/init/', key: 'cli/init' },
+	{ text: 'Build', header: true, anchor: true, slug: '/documentacao/devtools/cli/build/', key: 'cli/build' },
+	{ text: 'Deploy', header: true, anchor: true, slug: '/documentacao/devtools/cli/deploy/', key: 'cli/deploy' },	{ text: 'Dev', header: true, anchor: true, slug: '/documentacao/produtos/azion-cli/dev-comando/', key: 'cli/localDev' },
 	{ text: 'Link', header: true, anchor: true, slug: '/documentacao/produtos/azion-cli/link-comando/', key: 'cli/link' },
 	{ text: 'Login', header: true, anchor: true, slug: '/documentacao/produtos/cli/login/', key: 'cli/login' },
 	{ text: 'Logout', header: true, anchor: true, slug: '/documentacao/produtos/cli/logout/', key: 'cli/logout' },
