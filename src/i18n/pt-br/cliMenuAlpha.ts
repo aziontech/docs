f4aa6c58fcde157a9811a25e073db168ee14af11
Gabriel França <119522814+gabriel-azion@users.noreply.github.com>
2024-06-03 16:46:05 -0300

    feat: add config files to cli menu (#986)

--- src/i18n/pt-br/cliMenuAlpha.ts
+++ src/i18n/pt-br/cliMenuAlpha.ts
@@ -45,5 +45,12 @@ export default [
 		]
 	},
 
+	{
+        text: 'Configurações de projeto', header: true, type: 'learn', key: 'cli/configs', children: [
+            { text: 'azion.config.js', header: true, anchor: true, type: 'learn', slug: '/documentacao/devtools/cli/configs/azion-config-js/', key: 'cli/config/azion' },
+            { text: 'vulcan.config.js', header: true, anchor: true, type: 'learn', slug: '/documentacao/devtools/vulcan/config/', key: 'cli/config/vulcan' },
+        ]
+    },
+
 
 ] as const;
