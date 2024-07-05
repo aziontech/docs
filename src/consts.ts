5fd1509f047e0e42dff998e18a0dbfc17e17d7c7
ROBSON.JUNIOR <robsongajunior@users.noreply.github.com>
2024-07-05 17:41:19 -0300

    Fix seo (#1053)
    * fix: position anchor list
    
    * chore: clean file
    
    * allow
    
    * fix: hreflang, canonical, footer translatedPages
    
    * chore: remove commented code

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 export const SITE_TITLE = 'Azion';
-export const SITE_URL = 'http://localhost:4321';
-export const ASSETS_URL = 'http://localhost:4321';
+export const SITE_URL = 'https://www.azion.com';
+export const FAVICON_URL = 'https://www.azion.com/assets/images/favicon.png';
 export const FONTS_URL = 'https://fonts.azion.com';
-
 export const ALGOLIA_ID = 'PYJUZH6VNQ';
 export const ALGOLIA_APIKEY = '7c1795c333053265edd2aeb199745797';
