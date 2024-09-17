0527c2b94eddcdeaaf4e33e4f0f40cfa801b30b9
gabriel.azion <gabriel.franca@azion.com>
2024-09-17 11:22:13 -0300

    aa

---
+++ src/i18n/en/storeMenu.ts
@@ -0,0 +1,254 @@
+/**
+ * This configures the navigation sidebar.
+ * All other languages follow this ordering/structure and will fall back to
+ * English for any entries they haven't translated.
+ *
+ * - All entries MUST include `text` and `key`
+ * - Heading entries MUST include `header: true` and `type`
+ * - Link entries MUST include `slug` (which excludes the language code)
+ */
+export default [
+	{
+		text: 'Documentation',
+		header: true,
+		onlyMobile: true,
+		anchor: true,
+		type: 'learn',
+		slug: '/documentation/',
+		key: 'documentation',
+	},
+	{
+		text: 'Guides',
+		header: true,
+		onlyMobile: true,
+		anchor: true,
+		type: 'learn',
+		slug: '/documentation/products/guides/',
+		key: 'guides',
+	},
+	{
+		text: 'Dev Tools',
+		header: true,
+		onlyMobile: true,
+		anchor: true,
+		type: 'learn',
+		slug: '/documentation/devtools/',
+		key: 'devTools',
+	},
+
+	/// START HERE :::: DO NOT REMOVE the strings above, it's a work around for header on mobile ///
+	{
+		text: 'Overview',
+		header: true,
+		anchor: true,
+		slug: '/documentation/products/secure/overview/',
+		key: 'secureOverview',
+		hasLabel: 'menu.store',
+	},
+
+	{
+		text: 'Rest API Operations',
+		header: true,
+		type: 'learn',
+		key: 'edgeStorage/api',
+		hasLabel: 'menu.storage',
+		items: [
+			{
+				text: 'Create a bucket',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/create-bucket/',
+				key: 'createBucket',
+			},
+			{
+				text: 'List buckets',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/list-buckets/',
+				key: 'listBucket',
+			},
+			{
+				text: 'Update a bucket',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/update-buckets/',
+				key: 'updateBucket',
+			},
+			{
+				text: 'Delete a bucket',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/delete-buckets/',
+				key: 'deleteBucket',
+			},
+			{
+				text: 'Create an object',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/upload-object/',
+				key: 'postObject',
+			},
+			{
+				text: 'Update an object',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/update-object/',
+				key: 'putObject',
+			},
+			{
+				text: 'List objects',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/list-objects/',
+				key: 'listObjects',
+			},
+			{
+				text: 'Get object information',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/get-object/',
+				key: 'getObject',
+			},
+			{
+				text: 'Delete an object',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/delete-object/',
+				key: 'deleteObject',
+			},
+			{
+				text: 'Delete multiple objects',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/delete-objects/',
+				key: 'deleteObjects',
+			},
+			{
+				text: 'Use a bucket as origin',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/use-bucket-as-origin/',
+				key: 'useBucketAsOrigin',
+			},
+		],
+	},
+
+	{
+		text: 'S3 Protocol Operations',
+		header: true,
+		type: 'learn',
+		key: 'edgeStorage/s3',
+		items: [
+			{
+				text: 'Leverage S3 compatibility',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/storage/head-object/',
+				key: 'headObject',
+			},
+		],
+	},
+
+	{
+		text: 'Manage databases',
+		header: true,
+		type: 'learn',
+		hasLabel: 'menu.edgeSQL',
+		key: 'edgeSQL/databases',
+		items: [
+			{
+				text: 'Create a database',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/sql/create-database/',
+				key: 'createDatabase',
+			},
+			{
+				text: 'List databases',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/sql/list-databases/',
+				key: 'listDatabases',
+			},
+			{
+				text: 'Retrieve database info',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/sql/retrieve-database-info/',
+				key: 'retrieveDatabaseInfo',
+			},
+			{
+				text: 'Delete a database',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/sql/delete-database/',
+				key: 'deleteDatabase',
+			},
+		],
+	},
+
+	{
+		text: 'Work with tables',
+		header: true,
+		type: 'learn',
+		key: 'edgeSQL/tables',
+		items: [
+			{
+				text: 'Create a table',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/sql/create-table/',
+				key: 'createTable',
+			},
+			{
+				text: 'Insert into a table',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/sql/insert-into-table/',
+				key: 'insertIntoTable',
+			},
+			{
+				text: 'Query table rows',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/sql/query-table-rows/',
+				key: 'queryTableRows',
+			},
+			{
+				text: 'Update a record',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/sql/update-record/',
+				key: 'updateRecord',
+			},
+			{
+				text: 'Delete a record',
+				header: true,
+				anchor: true,
+				type: 'learn',
+				slug: '/documentation/products/store/sql/delete-record/',
+				key: 'deleteRecord',
+			},
+		],
+	},
+] as const;
