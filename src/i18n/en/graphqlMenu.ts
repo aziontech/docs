11b4465ec92ae1fdaa00504418620b53fab1e8f2
ROBSON.JUNIOR <robsongajunior@users.noreply.github.com>
2024-10-10 15:02:40 -0300

    Revert "[EDU-5503] feature: add guide - How to query the top URLs impacted by…"
    This reverts commit 4d49b4f2c41476cde8b65c97fbb48d4a6d99dbe6.

--- src/i18n/en/graphqlMenu.ts
+++ src/i18n/en/graphqlMenu.ts
@@ -37,8 +37,7 @@
 		{ text: 'How to query aggregated data', slug: '/documentation/products/guides/graphql-aggregated-data', key: 'guides/graphql-aggregated-data' },
 		{ text: 'How to select Top X queries', slug: '/documentation/products/guides/graphql-top-x-query', key: 'guides/graphql-top-x' },
 		{ text: 'How to query Connected Users data', slug: '/documentation/products/guides/query-connected-users-data-with-graphql/', key: 'guides/connected-users-graphql' },
-		{ text: 'How to query Bot Manager data', slug: '/documentation/products/guides/query-bot-manager-data-with-graphql/', key: 'guides/bot-data-graphql' },
-		{ text: 'How to query the top URLs impacted by bots', slug: '/documentation/products/guides/query-bot-manager-breakdown-data-with-graphql/', key: 'guides/bot-breakdown-data-graphql' }
+		{ text: 'How to query Bot Manager data', slug: '/documentation/products/guides/query-bot-manager-data-with-graphql/', key: 'guides/bot-data-graphql' }
 	] },
 
 
