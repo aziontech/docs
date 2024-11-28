823d9184e225be9c268f98b8e3258c4c354a0258
Robson Junior <robson.ga.junior@gmail.com>
2024-11-28 02:50:00 -0300

    chore: disable ripple

--- src/vue.config.js
+++ src/vue.config.js
@@ -3,7 +3,9 @@ import Tooltip from 'primevue/tooltip';
 import InstantSearch from 'vue-instantsearch/vue3/es';
 
 export default (app) => {
-  app.use(PrimeVue);
-  app.use(InstantSearch);
-  app.directive('tooltip', Tooltip);
+	app.use(PrimeVue, {
+		ripple: false
+	});
+	app.use(InstantSearch);
+	app.directive('tooltip', Tooltip);
 }
