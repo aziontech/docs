3cbb22331458e185396431bd14a123c1250aee21
Robson Junior <robson.ga.junior@gmail.com>
2025-01-26 10:55:02 -0300

    feat: semrush backend split module

--- backend/semrush-replace-url/index.js
+++ backend/semrush-replace-url/index.js
@@ -1,64 +1,42 @@
-import { parse } from 'csv-parse'
-import fs from 'fs'
-import { createReadStream } from 'fs'
-import path from 'path'
 import matter from 'gray-matter'
+import { promises as fs } from 'fs';
+import path from 'path'
 
+import { read as readcsv } from './helpers/csv.js';
+import { removeHostFromUrl, removeHostAndLangFromUrl, wwwazioncom, isFromRoot } from './helpers/url.js';
 
 let counterFoundLinks = 0
 let PATH = {
 	csv: './www.azion.com_permanent_redirects_20241225.csv',
 	docs: `${process.env.OLDPWD}/src/content/docs`
 }
-const wwwazioncom = 'https://www.azion.com'
 
 function findReplace(content, oldUrl, newUrl) {
 	return content.replace(oldUrl, newUrl)
 }
 
-async function loadRedirects() {
-	const redirects = []
-	const parser = createReadStream(PATH.csv).pipe(
-		parse({
-			columns: true,
-			skip_empty_lines: true,
-		})
-	)
-
-	for await (const record of parser) {
-		redirects.push({
-			page: record.page,
-			initialUrl: record.initial_url,
-			destinationUrl: record.destination_url,
-			statusCode: record.status,
-			discovered: record.discovered
-		})
-	}
-
-	return redirects
-}
-
-
 async function processFile(filePath, redirects) {
-	fs.readFile(filePath, async (err, content) => {
-		if(err) {
-			console.error(err)
-			return
-		}
+	try {
+		let fileModified = false;
 
+		const content = await fs.readFile(filePath, 'utf-8');
 		const { data, content: markdownContent } = matter(content)
 		const utf8Content = Buffer.from(content).toString('utf-8')
 
 		for (const item of redirects) {
-			const pagePermalink = item.page.replace(wwwazioncom, '').replace('/pt-br', '').replace('/en', '')
-			const url30x = item.initialUrl === wwwazioncom ? wwwazioncom : item.initialUrl
-			const url200 = item.destinationUrl
-			const isRoot = url30x === wwwazioncom
+			const pagePermalink = removeHostFromUrl(item.page)
+			const url30x = isFromRoot(item.initialUrl) ? wwwazioncom() : removeHostFromUrl(item.initialUrl);
+			const url200 = removeHostFromUrl(item.destinationUrl)
+			const isRoot = isFromRoot(url30x)
 			const rgx = new RegExp(`\\(${url30x}\\)`, 'g')
 			const contentMatch = utf8Content.match(rgx)
 
 			if(!contentMatch) continue
+			
 			counterFoundLinks++
+			fileModified = true
+
+			const newContent = findReplace(utf8Content, isRoot ? /\\(https\:\/\/www\.azion\.com\/\\)/ : rgx, `(${url200})`)
 
 			console.log(`{
 			isRoot: ${isRoot},
@@ -66,49 +44,57 @@ async function processFile(filePath, redirects) {
 			file: ${filePath},
 			permalink: ${data.permalink},
 			rgx: ${rgx},
+			initialUrl: ${item.initialUrl},
 			url30x: ${url30x},
 			url200: ${url200},
 			contentMatch: ${contentMatch},
 			contentMatchCount: ${contentMatch.length},
 			processedCount: ${counterFoundLinks}
 			}`)
-
-			const newContent = findReplace(utf8Content, isRoot ? /\\(https\:\/\/www\.azion\.com\/\\)/ : rgx, `(${url200})`)
-			await fs.writeFile(filePath, newContent, async (err) => {
-				if(err) throw err
-				console.log(`[OK] ${filePath} updated`)
-			})
-		}
-	})
-}
-
-function processDirectory(directory, redirects) {
-	fs.readdir(directory, { withFileTypes: true }, (err, entries) => {
-		if (err) {
-			console.error('[ERROR] directory can not be readed:', err)
-			return
-		}
-
-		for (const entry of entries) {
-			const fullPath = path.join(directory, entry.name)
-
-			if(entry.isDirectory()) {
-				processDirectory(fullPath, redirects)
-			} else if (entry.isFile()) {
-				processFile(fullPath, redirects)
-			} else {
-				console.error(`[ERROR] ${fullPath} is not a file or directory`)
+			
+			if(fileModified){
+				await fs.writeFile(filePath, newContent)
+				console.log(`[OK] ${filePath} updated`);
 			}
 		}
-	})
+	}
+	catch (error) {
+		console.error(`[ERROR] Processing file ${filePath}:`, error);
+	}
+}
+
+async function processDirectory(directory, redirects) {
+  try {
+    const entries = await fs.readdir(directory, { withFileTypes: true });
+    const processingPromises = entries.map(async (entry) => {
+      const fullPath = path.join(directory, entry.name);
+      
+      if (entry.isDirectory()) {
+        return processDirectory(fullPath, redirects);
+      } else if (entry.isFile()) {
+        return processFile(fullPath, redirects);
+      } else {
+        console.warn(`[WARN] ${fullPath} is not a file or directory`);
+      }
+    });
+
+    await Promise.all(processingPromises);
+  } catch (error) {
+    console.error(`[ERROR] Processing directory ${directory}:`, error);
+  }
 }
 
 async function main() {
 	try {
-		const redirects = await loadRedirects()
-		processDirectory(PATH.docs, redirects)
+		console.log('[INFO] Loading redirects...');
+    const redirects = await readcsv(PATH.csv);
+    console.log(`[INFO] Loaded ${redirects.length} redirects`);
+    
+    console.log('[INFO] Starting directory processing...');
+		await processDirectory(PATH.docs, redirects)
+		console.log(`[INFO] Processing complete. Found and replaced ${counterFoundLinks} links`);
 	} catch (error) {
-		console.error('[ERROR] ', error)
+		console.error('[ERROR] Fatal error:', error);
 		process.exit(1)
 	}
 }
