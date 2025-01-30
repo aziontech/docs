d31f905c38e50cb0927b57efeb5660746988ac77
guiafonso-ol <guilherme.oliveira@azion.com>
2025-01-30 09:53:37 -0300

    feat: update menus

--- src/i18n/pt-br/libMenu.ts
+++ src/i18n/pt-br/libMenu.ts
@@ -17,5 +17,15 @@ export default [
 	{ text: 'Visão Geral', header: true, anchor: true, type: 'learn', key: 'overview', slug: '/documentacao/produtos/azion-lib/overview' },
 	{ text: 'Uso', header: true, type: 'learn', key: 'usage', items: [
 		{ text: 'Client', slug: '/documentacao/produtos/azion-lib/client/', key: 'usage/`client`' },
+		{ text: 'Config', key: 'usage/config', slug: '/documentacao/produtos/azion-lib/config/' },
+		{ text: 'Cookies', header: true, key: 'usage/cookies', slug: '/documentacao/produtos/azion-lib/cookies/' },
+		{ text: 'Domains', header: true, key: 'usage/domains', slug: '/documentacao/produtos/azion-lib/domains/' },
+		{ text: 'JWT', header: true, key: 'usage/jwt', slug: '/documentacao/produtos/azion-lib/jwt/' },
+		{ text: 'Purge', header: true, key: 'usage/purge', slug: '/documentacao/produtos/azion-lib/purge/' },
+		{ text: 'SQL', header: true, key: 'usage/sql', slug: '/documentacao/produtos/azion-lib/sql/' },
+		{ text: 'Storage', header: true, key: 'usage/store', slug: '/documentacao/produtos/azion-lib/storage/' },
+		{ text: 'Types', header: true, key: 'usage/types', slug: '/documentacao/produtos/azion-lib/types/' },
+		{ text: 'Utils', header: true, key: 'usage/utils', slug: '/documentacao/produtos/azion-lib/utils/' },
+		{ text: 'WASM Image Processor', header: true, key: 'usage/wasm-image-processor', slug: '/documentacao/produtos/azion-lib/wasm-image-processor/' },
 	] },
 ] as const;
