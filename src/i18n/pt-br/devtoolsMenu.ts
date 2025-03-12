502e879318727e20edfc8b648397bbf7523d6d69
Luiza  Vargas do Santos <luiza.santos@FVFFPG1WQ6L4.local>
2025-03-12 08:59:39 -0300

    fix:reorder devtools sidebar menu

--- src/i18n/pt-br/devtoolsMenu.ts
+++ src/i18n/pt-br/devtoolsMenu.ts
@@ -13,13 +13,13 @@
 	{ text: 'Dev Tools',header: true, onlyMobile: true, anchor: true, slug: '/documentacao/produtos/dev-tools/', key: 'devTools' },
 
 	/// START HERE :::: DO NOT REMOVE the strings above, it's a work around for header on mobile /// 
-
-	{ text: 'CLI', header: true, anchor: true, type: 'learn', key: 'devtools/cli', slug: 'documentacao/produtos/azion-cli/visao-geral', hasLabel:'menu.devTools' },
-	{ text: 'Azion Lib', header: true, anchor: true, type: 'learn', key: 'devtools/azionlib', slug: '/documentacao/produtos/azion-lib/visao-geral/' },
-	{ text: 'API', header: true, anchor: true, type: 'learn', key: 'devtools/api', slug: 'https://api.azion.com/' },
+	{ text: 'API', header: true, anchor: true, type: 'learn', key: 'devtools/api', slug: 'https://api.azion.com/', hasLabel:'menu.devTools' },
 	{ text: 'API GraphQL', header: true, anchor: true, type: 'learn', slug: '/documentacao/devtools/graphql-api/', key: 'devtools/graphQL' },
+	{ text: 'Azion Lib', header: true, anchor: true, type: 'learn', key: 'devtools/azionlib', slug: '/documentacao/produtos/azion-lib/visao-geral/' },
+	{ text: 'CLI', header: true, anchor: true, type: 'learn', key: 'devtools/cli', slug: 'documentacao/produtos/azion-cli/visao-geral' },
+	{ text: 'Console Kit', header: true, anchor: true, type: 'learn', slug: '/documentacao/devtools/console-kit/', key: 'devtools/consoleKit' },
 	{ text: 'SDK',header: true, anchor: true, type: 'learn', slug: '/documentacao/devtools/sdk/go/', key: 'devtools/sdk' },
 	{ text: 'Terraform', header: true, anchor: true, type: 'learn', slug: '/documentacao/produtos/terraform-provider/', key: 'devtools/terraform' },
-	{ text: 'Console Kit', header: true, anchor: true, type: 'learn', slug: '/documentacao/devtools/console-kit/', key: 'devtools/consoleKit' },
+
 
 ] as const;
