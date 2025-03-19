b78f7060b2c4fc04b4e03805884c14699d8b6ce6
Mariana Bellorín Aguilera <89138244+MarianaAguilera@users.noreply.github.com>
2025-03-19 10:34:33 -0500

    feature: add link to menu and guides page - PT

--- src/i18n/pt-br/graphqlMenu.ts
+++ src/i18n/pt-br/graphqlMenu.ts
@@ -45,7 +45,9 @@
 		{ text: 'Como identificar os principais IPs gerando tráfego de ataque', slug: '/documentacao/produtos/guias/consultar-top-ips-gerando-trafego-de-ataque-com-graphql/', key: 'guides/top-ips-graphql' },
 		{ text: 'Como identificar os principais ataques', slug: '/documentacao/produtos/guias/consultar-top-attacks-com-graphql/', key: 'guides/bot-top-attacks-graphql' },
 		{ text: 'Como consultar dados de uso de Edge Functions', slug: '/documentacao/produtos/guias/consultar-dados-de-uso-edge-functions-com-graphql/', key: 'guides/edge-functions-usage-data' },
-		{ text: 'Como consultar dados de uso do Image Processor', slug: '/documentacao/produtos/guias/consultar-dados-de-uso-image-processor-com-graphql/', key: 'guides/image-processor-usage-data' }
+		{ text: 'Como consultar dados de uso do Image Processor', slug: '/documentacao/produtos/guias/consultar-dados-de-uso-image-processor-com-graphql/', key: 'guides/image-processor-usage-data' },
+		{ text: 'Como consultar dados de uso do Edge Application', slug: '/documentacao/produtos/guias/consultar-dados-de-uso-edge-application-com-graphql/', key: 'guides/edge-application-usage-data' }
+
 
 	] },
 
