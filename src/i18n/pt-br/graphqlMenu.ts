f8b2c00e14ba789a20d59faba565d24861847b4f
Mariana Bellorín Aguilera <89138244+MarianaAguilera@users.noreply.github.com>
2025-03-20 09:29:00 -0300

    [EDU-6313] [EDU-6314] feature: add new guides related to the Consumption dataset (#1554)
    * feature: add query data stream data guide - EN
    
    * feature: add data stream consumption guide - PT
    
    * feature: add data stream guide to menu and guides page EN/PT
    
    * feature: add Tiered Cache consumption guide - EN
    
    * feature: add tiered cache consumption guide - PT
    
    * feature: add link Tiered Cache guide to menu and guides page - EN/PT
    
    * fix: adjust links - PT guides
    
    * Apply suggestions from code review
    
    Co-authored-by: MarianaReisAlcantara <mariana.alcantara@azion.com>
    Co-authored-by: Guilherme Afonso Oliveira  <guilherme.oliveira@azion.com>
    
    ---------
    
    Co-authored-by: MarianaReisAlcantara <mariana.alcantara@azion.com>
    Co-authored-by: Guilherme Afonso Oliveira <guilherme.oliveira@azion.com>

--- src/i18n/pt-br/graphqlMenu.ts
+++ src/i18n/pt-br/graphqlMenu.ts
@@ -45,7 +45,9 @@
 		{ text: 'Como identificar os principais IPs gerando tráfego de ataque', slug: '/documentacao/produtos/guias/consultar-top-ips-gerando-trafego-de-ataque-com-graphql/', key: 'guides/top-ips-graphql' },
 		{ text: 'Como identificar os principais ataques', slug: '/documentacao/produtos/guias/consultar-top-attacks-com-graphql/', key: 'guides/bot-top-attacks-graphql' },
 		{ text: 'Como consultar dados de uso de Edge Functions', slug: '/documentacao/produtos/guias/consultar-dados-de-uso-edge-functions-com-graphql/', key: 'guides/edge-functions-usage-data' },
-		{ text: 'Como consultar dados de uso do Image Processor', slug: '/documentacao/produtos/guias/consultar-dados-de-uso-image-processor-com-graphql/', key: 'guides/image-processor-usage-data' }
+		{ text: 'Como consultar dados de uso do Image Processor', slug: '/documentacao/produtos/guias/consultar-dados-de-uso-image-processor-com-graphql/', key: 'guides/image-processor-usage-data' },
+		{ text: 'Como consultar dados de uso do Data Stream', slug: '/documentacao/produtos/guias/consultar-dados-de-uso-data-stream-com-graphql/', key: 'guides/data-stream-usage-data' },
+		{ text: 'Como consultar dados de uso do Tiered Cache', slug: '/documentacao/produtos/guias/consultar-dados-de-uso-tiered-cache-com-graphql/', key: 'guides/tiered-cache-usage-data' }
 
 	] },
 
