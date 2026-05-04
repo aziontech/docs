0bb476049795e9728e4ff280090146f005706a45
Bruno Germano <bruno.andrade@azion.com>
2026-05-04 11:11:52 -0300

    Update BASE_URL for ogImage function

--- src/util/ogImage.ts
+++ src/util/ogImage.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://8v0frmzqj20.map.azionedge.net';
+const BASE_URL = 'https://anp9wamula0.map.azionedge.nett';
 
 export default function ogImage(title: string, description: string, type: string = 'dotGrid') {
 	return `${BASE_URL}?title=${encodeURIComponent(title)}&subtitle=${encodeURIComponent(
