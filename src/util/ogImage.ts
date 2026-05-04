ddb534e115aa4ed57adb281ad812ef3acf208341
Bruno Germano <bruno.germano@gmail.com>
2026-05-04 11:14:52 -0300

    fix: typo

--- src/util/ogImage.ts
+++ src/util/ogImage.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://anp9wamula0.map.azionedge.nett';
+const BASE_URL = 'https://anp9wamula0.map.azionedge.net';
 
 export default function ogImage(title: string, description: string, type: string = 'dotGrid') {
 	return `${BASE_URL}?title=${encodeURIComponent(title)}&subtitle=${encodeURIComponent(
